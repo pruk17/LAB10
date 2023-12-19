ab51ca4314976e8331e1d7b40e5f1fc5297797e2
Chaiyapruk Parinyanuntakan <chaiyapruk6494@gmail.com>
2023-12-19 16:29:36 +0700

    add lab10_3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-#include 
 #include <iomanip>
-
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
-
-int main(){
-
-    ifstream source;
-    ofstream dest;
-    source.open(score.txt);
-    cout << "Number of data = ";
-    cout << setprecision(3);
-    cout << "Mean = ";
-    cout << "Standard deviation = ";
+int main()
+{
+        int count = 0;
+        float sum = 0, sum_square, mean;
+        float mean_square, meanSquareSum;
+        string textline;
+        ifstream source("score.txt");
+        while (getline(source, textline))
+        {
+            sum += atof(textline.c_str());
+            sum_square += pow(atof(textline.c_str()), 2);
+            count++;
+        }
+        cout << "Number of data = " << count << "\n";
+        cout << setprecision(3);
+        mean = sum/count;
+        mean_square = pow(mean, 2);
+        meanSquareSum = sum_square/count;
+        cout << "Mean = " << mean << "\n";
+        cout << "Standard deviation = " << sqrt(meanSquareSum-mean_square);
 }
